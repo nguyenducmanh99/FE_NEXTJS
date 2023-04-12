dbf12e301a2856e7532b32c9d3cb308ecf8303cf
Manhnd@mor.com <nguyenducmanh02121999@gmail.com>
2023-04-12 08:58:16 +0700

    add error page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-
+import 'bootstrap/dist/css/bootstrap.css';
+import {useEffect} from "react"
+import ErrorBoundary from '@/pages/error'
+import { Roboto } from 'next/font/google'
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+})
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  useEffect(()=>{
+    require("bootstrap/dist/js/bootstrap");
+  },[]);
+
+  return (
+      <main className={roboto.className}>
+        <ErrorBoundary>
+       <Component {...pageProps} />
+       </ErrorBoundary>
+      </main> 
+    )
 }
