c757e4f7b486d1393d8c99acaf8024e1c62b1148
Manhnd@mor.com <nguyenducmanh02121999@gmail.com>
2023-04-13 15:25:18 +0700

    add prettierrc

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,23 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import 'bootstrap/dist/css/bootstrap.css';
-import {useEffect} from "react"
-import ErrorBoundary from '@/pages/error'
-import { Roboto } from 'next/font/google'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import "bootstrap/dist/css/bootstrap.css";
+import { useEffect } from "react";
+import ErrorBoundary from "@/pages/error";
+import { Roboto } from "next/font/google";
 const roboto = Roboto({
-  weight: '400',
-  subsets: ['latin'],
-})
+  weight: "400",
+  subsets: ["latin"],
+});
 export default function App({ Component, pageProps }: AppProps) {
-
-  useEffect(()=>{
+  useEffect(() => {
     require("bootstrap/dist/js/bootstrap");
-  },[]);
+  }, []);
 
   return (
-      <main className={roboto.className}>
-        <ErrorBoundary>
-       <Component {...pageProps} />
-       </ErrorBoundary>
-      </main> 
-    )
+    <main className={roboto.className}>
+      <ErrorBoundary>
+        <Component {...pageProps} />
+      </ErrorBoundary>
+    </main>
+  );
 }
